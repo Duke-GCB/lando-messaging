e4e248d82fbb2a1a79742d8896a2f3975b8f4c41
John Bradley <johnbradley2008@gmail.com>
2016-12-07 15:18:52 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lando_messaging',
+      version='0.1',
+      description='Lando workflow messaging component',
+      url='https://github.com/Duke-GCB/lando-messaging',
+      author='John Bradley',
+      author_email='john.bradley@duke.edu',
+      license='MIT',
+      packages=['pika==0.10.0'],
+     )
