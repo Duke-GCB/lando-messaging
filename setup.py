70a4212a3198163320411253807ea8c186dab1f2
John Bradley <johnbradley2008@gmail.com>
2017-03-22 10:28:01 -0400

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lando_messaging',
-      version='0.3',
+      version='0.4',
       description='Lando workflow messaging component',
       url='https://github.com/Duke-GCB/lando-messaging',
       author='John Bradley',
